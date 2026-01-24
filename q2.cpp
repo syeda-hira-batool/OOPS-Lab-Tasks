0c0cea93202542ac6a1f36cadca5efa649e1477c
syeda-hira-batool <ic.shirabatool09@gmail.com>
2026-01-24 15:27:31 +0500

    Question 2: Scenario: You're creating a simple system for a small library to track book information. Task: Write a C++ program using structures to: 1. Create a Book structure with: ▪ Book ID (integer) ▪ Book Title (string) ▪ Author Name (string) ▪ Price (float) 2. Take input for 3 books 3. Display all book information 4. Calculate and display the total price of all books

---
+++ q2.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Book {
+    int ID;
+    string Booktitle;
+    string AuthorName;
+    float Price;
+};
+
+int main() {
+
+    Book b1, b2, b3;
+	
+    cout << "Enter the Book ID for book 1: ";
+    cin >> b1.ID;
+    cin.ignore();
+
+    cout << "Enter the Book Author Name for book 1: ";
+    getline(cin, b1.AuthorName);
+
+    cout << "Enter the Book Title for book 1: ";
+    getline(cin, b1.Booktitle);
+
+    cout << "Enter the Book price for book 1: ";
+    cin >> b1.Price;
+
+    cout << "\nEnter the Book ID for book 2: ";
+    cin >> b2.ID;
+    cin.ignore();
+
+    cout << "Enter the Book Author Name for book 2: ";
+    getline(cin, b2.AuthorName);
+
+    cout << "Enter the Book Title for book 2: ";
+    getline(cin, b2.Booktitle);
+
+    cout << "Enter the Book price for book 2: ";
+    cin >> b2.Price;
+
+    cout << "\nEnter the Book ID for book 3: ";
+    cin >> b3.ID;
+    cin.ignore();
+
+    cout << "Enter the Book Author Name for book 3: ";
+    getline(cin, b3.AuthorName);
+
+    cout << "Enter the Book Title for book 3: ";
+    getline(cin, b3.Booktitle);
+
+    cout << "Enter the Book price for book 3: ";
+    cin >> b3.Price;
+
+    cout << "\nDisplaying info:\n";
+	cout << "\nBook 1:";
+    cout << "\nID: " << b1.ID;
+    cout << "\nAuthor: " << b1.AuthorName;
+    cout << "\nTitle: " << b1.Booktitle;
+    cout << "\nPrice: " << b1.Price << endl;
+	cout << "\nBook 2:";
+    cout << "\nID: " << b2.ID;
+    cout << "\nAuthor: " << b2.AuthorName;
+    cout << "\nTitle: " << b2.Booktitle;
+    cout << "\nPrice: " << b2.Price << endl;
+	cout << "\nBook 3:";
+    cout << "\nID: " << b3.ID;
+    cout << "\nAuthor: " << b3.AuthorName;
+    cout << "\nTitle: " << b3.Booktitle;
+    cout << "\nPrice: " << b3.Price << endl;
+
+    float total = b1.Price + b2.Price + b3.Price;
+    cout << "\nTotal Price is: " << total << endl;
+
+    return 0;
+}

a60a9a9e1c5bffc16a13a73648988be47ce5b387
syeda-hira-batool <ic.shirabatool09@gmail.com>
2026-01-24 15:28:25 +0500

    Question:3 Scenario: A college wants to analyze student performance across multiple semesters. Task: Write a C++ program that: 1. Uses a structure Student containing: ▪ Roll Number ▪ Name ▪ Marks in 3 subjects (array of 3 integers) ▪ CGPA (float) 2. Uses dynamic memory allocation to create an array of Student structures (ask user how many students to enter) 3. Write functions for: ▪ Input student details ▪ Calculate CGPA (Assume: CGPA = total marks/30) ▪ Display all students with CGPA ≥ 3.0 ▪ Find and display top performer 4. Use pointers to manipulate the student array 5. Properly deallocate memory at the end

---
+++ q3.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    int rollNumber;
+    string name;
+    int marks[3];
+    float cgpa;
+};
+
+void input(Student* students, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "\nEnter Student " << i + 1 <<" roll number: "<< endl;
+        cin >> students[i].rollNumber;
+        cin.ignore(); 
+        cout << "\nEnter Student " << i + 1 <<" name: "<< endl;
+        getline(cin, students[i].name);
+        cout << "\nEnter Student " << i + 1 <<" marks for 3 subjects: "<< endl;
+        for (int j = 0; j < 3; j++) {
+            cout << "Subject " << j + 1 << ": ";
+            cin >> students[i].marks[j];
+        }
+    }
+}
+
+void CGPA(Student* students, int n) {
+    for (int i = 0; i < n; i++) {
+        int total = 0;
+        for (int j = 0; j < 3; j++) {
+            total += students[i].marks[j];
+        }
+        students[i].cgpa = total / 30.0; 
+    }
+}
+
+
+void EligibleStudents(Student* students, int n) {
+    cout << "\nStudents with CGPA >= 3.0:\n";
+    for (int i = 0; i < n; i++) {
+        if (students[i].cgpa >= 3.0) {
+            cout << "Name: " << students[i].name<< endl;
+        }
+    }
+}
+
+void TopPerformer(Student* students, int n) {
+	
+    float maxCgpa = students[0].cgpa;
+    for (int i = 1; i < n; i++) {
+        if (students[i].cgpa > maxCgpa) {
+            maxCgpa = students[i].cgpa;
+        }
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (students[i].cgpa == maxCgpa) {
+            count++;
+        }
+    }
+    cout << "\nTop Performer(s):\n";
+    if (count == n) {
+        cout << "All students are Top Performers\n";
+        for (int i = 0; i < n; i++) {
+            cout << "Name: " << students[i].name << endl;
+        }
+    }
+    else {
+        for (int i = 0; i < n; i++) {
+            if (students[i].cgpa == maxCgpa) {
+                cout << "Name: " << students[i].name << endl;
+            }
+        }
+    }
+}
+
+
+int main() {
+    int n;
+    cout << "Enter number of students: ";
+    cin >> n;
+
+    Student* students = new Student[n];
+
+    input(students, n);
+    CGPA(students, n);
+    EligibleStudents(students, n);
+    TopPerformer(students, n);
+
+
+    delete[] students;
+
+    return 0;
+}
